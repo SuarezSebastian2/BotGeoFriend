84f789be4b9f9860bb491fbd35f8f4c3dd846fb1
Sebastian Andres Suarez Guzman <suarezsebastian98@hotmail.com>
2022-11-10 15:09:50 -0500

    Agregar archivos de proyecto.

---
+++ GeoFriend/Bots/EchoBot.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.18.1
+
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GeoFriend.Bots
+{
+    public class EchoBot : ActivityHandler
+    {
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var name = turnContext.Activity.Text;
+            var f = System.IO.Path.Combine(System.Environment.CurrentDirectory, @"worldcities.csv");
+            var cd = new CountryData(f);
+            var cap = cd.GetCapital(name);
+            var replyText = cap == null
+                ? "I do not know this country"
+                : $"The capital of {name} is {cap}!";
+            await turnContext.SendActivityAsync(replyText);
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var welcomeText = "Hello. I am a teaching assistant bot that will help you learn geography. If you enter a country, I can tell you its capital.";
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
+                }
+            }
+        }
+    }
+}
